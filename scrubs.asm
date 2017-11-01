e8ea9287a934ca924b12a5a6baab0363bcca7c74
Derek Machuca <Derek_machuca@yahoo.com>
2017-11-01 14:51:46 -0500

    no scrubs

---
+++ scrubs.asm
@@ -0,0 +1,76 @@
+.text
+	li $a0, 56
+  	li $a1, 250
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	li $a0, 59
+  	li $a1, 250
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	li $a0, 64
+  	li $a1, 300
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	
+  	li $a0, 56
+  	li $a1, 250
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	li $a0, 59
+  	li $a1, 250
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	li $a0, 62
+  	li $a1, 300
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	
+  	li $a0, 55
+  	li $a1, 250
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	li $a0, 58
+  	li $a1, 250
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	li $a0, 61
+  	li $a1, 300
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	
+  	li $a0, 56
+  	li $a1, 250
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	li $a0, 59
+  	li $a1, 250
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
+  	li $a0, 62
+  	li $a1, 300
+  	li $a2, 32#
+ 	li $a3, 127
+ 	la $v0, 33
+  	syscall
